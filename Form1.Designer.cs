2756830692c29799f622b6ce6336e643887514e9
drvc93 <drvc_1993@hotmail.com>
2017-05-18 18:10:08 -0500

    18/05/17

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace QrGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.gvCodigos = new DevExpress.XtraGrid.GridControl();
+            this.gvCodigosLista = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.btnAddCod = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCodigos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCodigosLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(558, 211);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generar";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // gvCodigos
+            // 
+            this.gvCodigos.Location = new System.Drawing.Point(12, 61);
+            this.gvCodigos.MainView = this.gvCodigosLista;
+            this.gvCodigos.Name = "gvCodigos";
+            this.gvCodigos.Size = new System.Drawing.Size(526, 221);
+            this.gvCodigos.TabIndex = 1;
+            this.gvCodigos.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gvCodigosLista});
+            // 
+            // gvCodigosLista
+            // 
+            this.gvCodigosLista.GridControl = this.gvCodigos;
+            this.gvCodigosLista.Name = "gvCodigosLista";
+            this.gvCodigosLista.OptionsView.ShowGroupPanel = false;
+            // 
+            // txtCod
+            // 
+            this.txtCod.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCod.Location = new System.Drawing.Point(12, 35);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(280, 22);
+            this.txtCod.TabIndex = 2;
+            // 
+            // btnAddCod
+            // 
+            this.btnAddCod.Location = new System.Drawing.Point(305, 34);
+            this.btnAddCod.Name = "btnAddCod";
+            this.btnAddCod.Size = new System.Drawing.Size(75, 23);
+            this.btnAddCod.TabIndex = 3;
+            this.btnAddCod.Text = "Agregar";
+            this.btnAddCod.Click += new System.EventHandler(this.btnAddCod_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 294);
+            this.Controls.Add(this.btnAddCod);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.gvCodigos);
+            this.Controls.Add(this.btnGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gvCodigos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvCodigosLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private DevExpress.XtraGrid.GridControl gvCodigos;
+        private DevExpress.XtraGrid.Views.Grid.GridView gvCodigosLista;
+        private System.Windows.Forms.TextBox txtCod;
+        private DevExpress.XtraEditors.SimpleButton btnAddCod;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.QrCodeNet.Encoding.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace QrGenerator
+{
+    public partial class Form1 : Form
+    {
+        DataTable data;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+
+           // byte[] img = Convert.FromBase64String(
+            string sCod = "";
+
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                sCod = data.Rows[i]["CodigoFitrro"].ToString();
+                Bitmap b = GenerateQRCode(sCod, Color.Black, Color.White);
+                Image img = (Image)b;
+                img = resizeImage(img, 72, 72);
+                img.Save( "D:\\Imgsqr\\"+sCod+".Jpeg", ImageFormat.Jpeg);
+
+            }
+           /* SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                b.Save(dialog.FileName + ".png", ImageFormat.Png);
+            }*/
+        }
+
+        private Bitmap GenerateQRCode(string text, System.Drawing.Color DarkColor, System.Drawing.Color LightColor)
+        {
+            Gma.QrCodeNet.Encoding.QrEncoder Encoder = new Gma.QrCodeNet.Encoding.QrEncoder(Gma.QrCodeNet.Encoding.ErrorCorrectionLevel.H);
+            Gma.QrCodeNet.Encoding.QrCode Code = Encoder.Encode(text);
+            Bitmap TempBMP = new Bitmap(Code.Matrix.Width, Code.Matrix.Height);
+           // Bitmap TempBMP = new Bitmap(48, 48);
+            for (int X = 0; X <= Code.Matrix.Width - 1; X++)
+            {
+                for (int Y = 0; Y <= Code.Matrix.Height - 1; Y++)
+                {
+                    if (Code.Matrix.InternalArray[X, Y])
+                        TempBMP.SetPixel(X, Y, DarkColor);
+                    else
+                        TempBMP.SetPixel(X, Y, LightColor);
+                }
+            }
+            return TempBMP;
+        }
+
+        private void btnAddCod_Click(object sender, EventArgs e)
+        {
+
+            if (gvCodigosLista.DataRowCount <= 0)
+            {
+                data = CreateDatatable();
+                InsertData(1, txtCod.Text);
+
+            }
+            else
+            {
+                InsertData(data.Rows.Count + 1, txtCod.Text);
+            }
+
+            gvCodigos.DataSource = data;
+        }
+
+        public DataTable CreateDatatable()
+        {
+            DataTable table = new DataTable();
+
+            DataColumn col1 = new DataColumn("Numero");
+            DataColumn col2 = new DataColumn("CodigoFitrro");
+
+            //col1.DataType = System.Type.GetType("System.Int");
+            //col2.DataType = System.Type.GetType("System.String");
+
+            table.Columns.Add(col1);
+            table.Columns.Add(col2);
+
+            return table;
+
+        }
+
+        public void InsertData(int nro, string cod)
+        {   
+            int nrorow =  0 ;
+
+            if (data.Rows.Count > 0)
+            {
+                nrorow = data.Rows.Count + 1;
+            }
+
+            DataRow row = data.NewRow();
+            row[0] = nro;
+            row[1] = cod;
+            data.Rows.Add(row);
+        }
+
+
+        public static Image resizeImage(Image image, int new_height, int new_width)
+        {
+            Bitmap new_image = new Bitmap(new_width, new_height);
+            Graphics g = Graphics.FromImage((Image)new_image);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+            g.DrawImage(image, 0, 0, new_width, new_height);
+            return new_image;
+        }
+    }
+}
